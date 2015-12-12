a6072c0288cf32656b8222fb9d72af1c46718da1
Steven Zhang <shsteven1000@GMail.com>
2015-12-12 21:59:02 +0800

    Initial Commit

---
+++ MagicalAPI/MagicalAPI.h
@@ -0,0 +1,19 @@
+//
+//  MagicalAPI.h
+//  MagicalAPI
+//
+//  Created by Steven on 12/12/15.
+//  Copyright © 2015 Magical Bits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MagicalAPI.
+FOUNDATION_EXPORT double MagicalAPIVersionNumber;
+
+//! Project version string for MagicalAPI.
+FOUNDATION_EXPORT const unsigned char MagicalAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MagicalAPI/PublicHeader.h>
+
+
